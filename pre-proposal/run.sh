2bb54a6f7dde71cac5a98806cd7af87b1343b442
jovyan <jovyan@mybinder>
2022-04-11 14:16:49 +0000

    adding uml magic

--- pre-proposal/run.sh
+++ pre-proposal/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-jupyter nbconvert main.ipynb --to slides --output out
+jupyter nbconvert main.ipynb --to slides --TagRemovePreprocessor.remove_input_tags hide-code --output out
 mv out.slides.html index.html
